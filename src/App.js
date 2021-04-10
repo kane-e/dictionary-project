938d7fcd13e1d1f4b541e17fb041e498dbc8b46b
Emma Kane <kane2@pdx.edu>
2021-04-10 12:21:45 -0700

    Deleted default images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Dictionary
+      <header className="container">
+       
+        Dictionary App
+        
       </header>
     </div>
   );
